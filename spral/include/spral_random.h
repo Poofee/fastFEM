5795770c19610c64814b4a8e89a3cd53c8777a5f
poofee <poofee@qq.com>
2017-03-28 15:59:18 +0800

    添加spral的Linux库

---
+++ spral/include/spral_random.h
@@ -0,0 +1,25 @@
+#ifndef SPRAL_RANDOM_H
+#define SPRAL_RANDOM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+
+#define SPRAL_RANDOM_INITIAL_SEED 486502
+
+/* Generate a sample from Unif(-1,1) or Unif(0,1) */
+double spral_random_real(int *state, bool positive);
+/* Generate a sample from discrete Unif(1,...,n) */
+int spral_random_integer(int *state, int n);
+/* Generate a sample from discrete Unif(1,...,n) */
+long spral_random_long(int *state, long n);
+/* Generate a sample with equal probability of true or false */
+bool spral_random_logical(int *state);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif // SPRAL_RANDOM_H
---
+++ spral/include/spral_rutherford_boeing.h
@@ -0,0 +1,49 @@
+#ifndef SPRAL_RUTHERFORD_BOEING_H
+#define SPRAL_RUTHERFORD_BOEING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct spral_rb_read_options {
+   int array_base;
+   bool add_diagonal;
+   float extra_space;
+   int lwr_upr_full;
+   int values;
+};
+
+struct spral_rb_write_options {
+   int array_base;
+   char val_format[21];
+};
+
+void spral_rb_default_read_options(struct spral_rb_read_options *options);
+void spral_rb_default_write_options(struct spral_rb_write_options *options);
+int spral_rb_peek(const char *filename, int *m, int *n, long *nelt, long *nvar,
+      long *nval, enum spral_matrix_type *matrix_type, char *type_code,
+      char *title, char *identifier);
+int spral_rb_read(const char *filename, void **handle,
+      enum spral_matrix_type *matrix_type, int *m, int *n, long **ptr,
+      int **row, double **val, const struct spral_rb_read_options *options,
+      char *title, char *identifier, int *state);
+int spral_rb_read_ptr32(const char *filename, void **handle,
+      enum spral_matrix_type *matrix_type, int *m, int *n, int **ptr,
+      int **row, double **val, const struct spral_rb_read_options *options,
+      char *title, char *identifier, int *state);
+int spral_rb_write(const char *filename, enum spral_matrix_type matrix_type,
+      int m, int n, const long *ptr, const int *row, const double * val,
+      const struct spral_rb_write_options *options, const char *title,
+      const char *identifier);
+int spral_rb_write_ptr32(const char *filename,
+      enum spral_matrix_type matrix_type,
+      int m, int n, const int *ptr, const int *row, const double * val,
+      const struct spral_rb_write_options *options, const char *title,
+      const char *identifier);
+void spral_rb_free_handle(void **handle);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif // SPRAL_RUTHERFORD_BOEING_H
