c26c5fd048399e6fe242082a57bd2eab2ecc1ff7
poofee <poofee@qq.com>
2017-12-06 10:23:52 +0800

    修正了传输线法

--- matlab/单传输线法/singleTLM.m
+++ matlab/单传输线法/singleTLM.m
@@ -12,30 +12,26 @@ Y = [1/R1+1/R2+1/R4   -1/R2-1/R4;
    -1/R2-1/R4   1/R3+1/R2+1/R4];
 I = [I;0];
 V = Y\I;
-Z1 = 10;
-Z2 = 100;
-U1 = [0;0];
-i1 = [0;0];
-U2 = [0;0];
-i2 = [0;0];
+Z1 = 15;
+Z2 = 15;
+Vi = [0;0];
+Vr = [0;0];
 for i=1:50
     %incidence
     Y = [1/R1+1/R4+1/Z1   -1/R4;
    -1/R4   1/R3+1/R4+1/Z2];
-   I1 = I + [U2(1)/Z1-i2(1);U2(2)/Z2-i2(2)];
-   U1 = Y\I1;
-   i1(1) = U2(1)/Z1-i2(1)-U1(1)/Z1;
-   i1(2) = U2(2)/Z2-i2(2)-U1(2)/Z2;
+   I1 = I + 2*Vi.*[1/Z1;1/Z2];
+   V1 = Y\I1;
     %reflect
+    Vr = V1 - Vi;
     Y2 = [1/R2+1/Z1 -1/R2;
         -1/R2  1/R2+1/Z2];
-    I2 = [U1(1)/Z1-i1(1);U1(2)/Z2-i1(2)];
-    U2 = Y2\I2;
-    i2(1) = U1(1)/Z1-i1(1)-U2(1)/Z1;
-    i2(2) = U1(2)/Z2-i1(2)-U2(2)/Z2;
-    plot(i,U1(1),'ro');hold on;
-    plot(i,U1(2),'bo');hold on;
-    plot(i,U2(1),'r*');hold on;
-    plot(i,U2(2),'b*');hold on;
+    I2 = 2*Vr.*[1/Z1;1/Z2];
+    V2 = Y2\I2;
+    Vi = V2 - Vr;
+    plot(i,V1(1),'ro');hold on;
+    plot(i,V1(2),'bo');hold on;
+    plot(i,V2(1),'r*');hold on;
+    plot(i,V2(2),'b*');hold on;
 end
-U1
+V1
